d49380adb3594c1ff4e95efe2194de2c21dcd053
Serge <uaserge2@gmail.com>
2018-03-25 20:00:13 +0300

    hw_03 25.03.18 19:59

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,5 +1,6 @@
 DELETE FROM user_roles;
 DELETE FROM users;
+DELETE FROM meals;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
 
 INSERT INTO users (name, email, password) VALUES
@@ -9,3 +10,11 @@ INSERT INTO users (name, email, password) VALUES
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (dateTime, description, calories) VALUES
+  ('2015-06-04 22:00:00', 'Завтрак', 500),
+  ('2015-06-04 22:00:00', 'Ужин', 600),
+('2015-07-04 22:00:00', 'Завтрак', 500),
+('2015-07-04 22:00:00', 'Ужин', 600);
+
+
