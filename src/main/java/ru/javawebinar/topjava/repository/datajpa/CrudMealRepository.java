afb24b9cecc4718fbfb5a5b180e1b20ca946dda7
Serge-new <flgrbu8181>
2018-04-14 14:22:24 +0300

    14/04/2018 14.21

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudMealRepository.java
@@ -1,7 +1,56 @@
 package ru.javawebinar.topjava.repository.datajpa;
 
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
+import static javafx.scene.input.KeyCode.T;
+
+@Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
+
+    @Override
+    @Transactional
+    Meal save(Meal meal);
+
+    // false if meal do not belong to userId
+    @Transactional
+    @Modifying
+    @Query(name = Meal.DELETE)
+    boolean delete(@Param("id")int id, @Param("id")int userId);
+
+    // null if meal do not belong to userId
+    @Override
+    Optional<Meal> findById(Integer integer);
+
+
+    // ORDERED dateTime desc
+
+    @Transactional
+    @Modifying
+    @Query(name = Meal.ALL_SORTED)
+    List<Meal> findAllById(@Param("id")int userId);
+
+
+    // ORDERED dateTime desc
+
+    @Transactional
+    @Modifying
+    @Query(name = Meal.GET_BETWEEN)
+    List<Meal> findAllByTimeBetween(@Param("userId")int userId,
+                                    @Param("startDate")LocalDateTime startDate,
+                                    @Param("endDate")LocalDateTime endDate);
+
+
 }
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/DataJpaMealRepositoryImpl.java
@@ -3,6 +3,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDateTime;
@@ -14,28 +15,36 @@ public class DataJpaMealRepositoryImpl implements MealRepository {
     @Autowired
     private CrudMealRepository crudRepository;
 
+    @Autowired
+    private CrudUserRepository crudUserRepository;
+
     @Override
-    public Meal save(Meal Meal, int userId) {
-        return null;
+    public Meal save(Meal meal, int userId) {
+        if (!meal.isNew() && get(meal.getId(), userId) == null) {
+            return null;
+        }
+        meal.setUser(crudUserRepository.getOne(userId));
+        return crudRepository.save(meal);
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return crudRepository.delete(id,userId);
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        Meal meal = crudRepository.findById(id).get();
+        return meal != null && meal.getUser().getId() == userId ? meal : null;
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return crudRepository.findAllById(userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        return crudRepository.findAllByTimeBetween(userId, startDate, endDate);
     }
 }
