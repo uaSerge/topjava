919d6f7503686e7e8f8477e0ee649182df749971
uaSerge <uaserge2@gmail.com>
2018-05-06 22:42:26 +0300

    06.05.2018 06.05

--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -1,7 +1,37 @@
 package ru.javawebinar.topjava.web.meal;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.web.user.ProfileRestController;
 
-@Controller
+@RestController
+@RequestMapping(MealRestController.REST_URL)
 public class MealRestController extends AbstractMealController {
+    static final String REST_URL = "/rest/meals";
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public Meal get() {
+        return super.get(AuthorizedUser.id());
+    }
+
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void delete() {
+        super.delete(AuthorizedUser.id());
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void update(@RequestBody Meal meal) {
+        super.update(meal, AuthorizedUser.id());
+    }
+
+    @GetMapping(value = "/text")
+    public String testUTF() {
+        return "Русский текст";
+    }
 }
